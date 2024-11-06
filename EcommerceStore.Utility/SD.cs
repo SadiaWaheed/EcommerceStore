5f26f738f7f71a45cf7987f36e9773470ead5e32
Sadia Waheed <51211519+SadiaWaheed@users.noreply.github.com>
2024-11-06 21:41:33 +0300

    Adding Utility files

---
+++ EcommerceStore.Utility/SD.cs
@@ -0,0 +1,32 @@
+﻿namespace EcommerceStore.Utility
+{
+    public static class SD
+    {
+        public const string Role_User_Indi = "Individual";
+        public const string Role_User_Comp = "Company";
+        public const string Role_Admin = "Admin";
+        public const string Role_SuperAdmin = "SuperAdmin";
+
+        public const string StatusPending = "Pending";
+        public const string StatusApproved = "Approved";
+        public const string StatusInProcess = "Processing";
+        public const string StatusShipped = "Shipped";
+        public const string StatusCancelled = "Cancelled";
+        public const string StatusRefunded = "Refunded";
+
+        public const string PaymentStatusPending = "Pending";
+        public const string PaymentStatusApproved = "Approved";
+        public const string PaymentStatusDelayedPayment = "ApprovedForDelayedPayment";
+        public const string PaymentStatusRejected = "Rejected";
+        public const string PaymentStatusPaid = "paid";
+
+        public const string SessionCart = "SessionShoppingCart";
+
+        public const string ConfigType_Site = "Site";
+        public const string ConfigType_SocialLink = "Social Links";
+        public const string ConfigType_Logo = "Logo";
+        public const string ConfigType_Other = "Other";
+        public const string ConfigType_Slider = "Slider";
+
+    }
+}
